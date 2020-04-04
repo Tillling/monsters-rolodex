50a476842e1c03f9b9606e2b77ae62d9384419a3
Jonathan <jonathantilling89@gmail.com>
2020-04-04 14:47:32 +0200

    Updates

---
+++ precache-manifest.50b6ad10bc429795d7da25d3fac73472.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18b0555889542bc586774b5885f542df",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "67d8c3823a90dad8a343",
+    "url": "/monsters-rolodex/static/css/main.4afc8d50.chunk.css"
+  },
+  {
+    "revision": "a18d43196547948386f9",
+    "url": "/monsters-rolodex/static/js/2.8ebdf4f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.8ebdf4f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67d8c3823a90dad8a343",
+    "url": "/monsters-rolodex/static/js/main.0fc5f78c.chunk.js"
+  },
+  {
+    "revision": "345913ed4d11f5710073",
+    "url": "/monsters-rolodex/static/js/runtime-main.09132acd.js"
+  }
+]);
